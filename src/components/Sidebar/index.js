88d56815505b167e42127cf04fdac0ec5ff425ba
Diogo Merelles <diogo.faria.07@gmail.com>
2022-07-10 12:19:21 +0100

    updates portfolio skeleton

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,61 @@
+import './index.scss';
+import { Link, NavLink } from 'react-router-dom';
+import LogoS from '../../images/logo-s.png';
+import LogoSubtitle from '../../images/logo_sub.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
+const Sidebar = () => {
+    return(
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt="logo" />
+            <img className="sub-logo" src={LogoSubtitle} alt="logoSub" />
+        </Link>
+
+        <nav>
+
+          <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+
+
+          <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+
+
+          </NavLink><NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+
+        </nav>
+
+        <ul>
+            <li>
+                <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/diogo-merelles-faria/"
+                >   
+                <FontAwesomeIcon icon={faLinkedinIn} color= "#4d4d4e" />
+                </a>
+            </li>
+    
+            <li>
+                <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://github.com/"
+                >       
+                <FontAwesomeIcon icon={faGithub} color= "#4d4d4e" />
+                </a>
+            </li>
+        </ul>
+
+    </div>
+
+    )
+}
+
+export default Sidebar
