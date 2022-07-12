f729e33f013113c8f70810d671c313b399cd3086
Diogo Merelles <diogo.faria.07@gmail.com>
2022-07-12 18:55:17 +0100

    fix sidevar bug, added animations to home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import Layout from './components/Layout';
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <>
     <Routes>
-      <Route path="/" element={<Layout />} />
-        <Route index element={<Home />}></Route>
+      <Route path="/" element={<Layout />} >
+        <Route index element={<Home />} />
+          <Route element={<Sidebar />}>
+          
+
+          </Route>
+
+        </Route>
     </Routes>
     </>
   );
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,24 @@
-const Home = () => ('Hello World');
+import { Link } from "react-router-dom";
+import './index.scss';
 
-let armor = 80;
+const Home = () => {
+    return (
+
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>Hi, <br />I'm Diogo
+                <br />
+                Front-end Developer
+                </h1>
+                <Link to="/contact" className="flat-button">CONTACT ME</Link>
+            </div>
+        </div>
+
+    );
+}
+
+
+
+
 
 export default Home;
